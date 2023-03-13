b7202aff39193ead8ccd3914b067e4e1edc72bfe
Adhista Dera <deradhista@gmail.com>
2023-03-14 00:17:33 +0700

    Revert "9 update all lib and configuration"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
-import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 interface LayoutProps {
@@ -11,7 +10,6 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <div className="w-full h-screen bg-white overflow-auto">
       <Navbar />
       {children}
-      <Footer />
     </div>
   );
 };
